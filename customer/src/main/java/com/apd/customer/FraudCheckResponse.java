0a6a731f3b30d9a6410dbb371210cc7836e220c5
Ariel Pinto <arielp@goja.com>
2022-02-17 13:01:20 -0400

    add RestTemplate for communicate res api in customer and fraud module

---
+++ customer/src/main/java/com/apd/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.apd.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
--- fraud/src/main/java/com/apd/fraud/FraudController.java
+++ fraud/src/main/java/com/apd/fraud/FraudController.java
@@ -1,10 +1,11 @@
 package com.apd.fraud;
 
-import org.aspectj.lang.annotation.Pointcut;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
+@Slf4j
 public class FraudController {
 
     private final FraudCheckService fraudCheckService;
@@ -16,6 +17,7 @@ public class FraudController {
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerID) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerID);
+        log.info("fraud check request for customer {}", customerID);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
