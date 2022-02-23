e76684580537ff23341bf328414c3f7c851b7b53
Ariel Pinto <arielp@goja.com>
2022-02-23 14:57:14 -0400

    Update clientsFeign configuration resources and configure modules for support minikube

--- notification/src/main/java/com/apd/notification/NotificationApplication.java
+++ notification/src/main/java/com/apd/notification/NotificationApplication.java
@@ -4,7 +4,11 @@ import com.apd.amqp.RabbitMQMessageProducer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -12,6 +16,9 @@ import org.springframework.context.annotation.Bean;
                 "com.apd.amqp"
         }
 )
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
